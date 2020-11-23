e1dddee38018d6021fe73d7c353e05a651ccd69c
Allie Kim <allie0147@gmail.com>
2020-11-23 16:48:24 +0900

    Feat: add main web page

--- WebView_1123/AppDelegate.swift
+++ WebView_1123/AppDelegate.swift
@@ -20,14 +20,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
+        // Ca,lled when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
@@ -36,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     lazy var persistentContainer: NSPersistentContainer = {
         /*
-         The persistent container for the application. This implementation
+         The persistent container he application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
--- WebView_1123/ViewController.swift
+++ WebView_1123/ViewController.swift
@@ -2,18 +2,80 @@
 //  ViewController.swift
 //  WebView_1123
 //
-//  Created by ishift on 2020/11/23.
+//  Created by Allie Kim on 2020/11/23.
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    // url text
+    @IBOutlet weak var txtUrl: UITextField!
+    // web view
+    @IBOutlet weak var myWebView: WKWebView!
+    // indicator(spinner)
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadWebPage("http://2sam.net")
     }
 
+    func loadWebPage(_ url: String) {
+        let myUrl = URL(string: url)
+        let myReqeust = URLRequest(url: myUrl!)
+        myWebView.load(myReqeust)
+    }
+
+    // go button
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+
+
+    }
+
+    //site1 button
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+
+    }
+
+    //site2 button
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+
+
+    }
+
+    // html button
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+
+
+    }
+
+    //file button
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+
+
+    }
+
+    // stop button
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+
+
+    }
+
+    // refresh button
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+
+    }
+
+    // rewind button
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+
+    }
+
+    // fast-forward button
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+
+    }
 
 }
-
